a81f5353acd0217b8de198b4d5241b4d6c5915fb
loserydigsthehole <tassostheo11@gmail.com>
2025-06-29 23:04:10 -0400

    Update main.js
    fixed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
+// routes.js should export your route definitions like this:
+// export default [{ path: '/', component: Home }, ...]
 import routes from './routes.js';
 
-export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) || false,
+// Global reactive store
+const store = Vue.reactive({
+    dark: JSON.parse(localStorage.getItem('dark') || 'false'),
     toggleDark() {
         this.dark = !this.dark;
         localStorage.setItem('dark', JSON.stringify(this.dark));
     },
 });
 
+// Create Vue app
 const app = Vue.createApp({
-    data: () => ({ store }),
+    setup() {
+        return { store };
+    }
 });
+
+// Create router
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes,
 });
 
+// Install router into app
 app.use(router);
 
+// Mount app to DOM
 app.mount('#app');
